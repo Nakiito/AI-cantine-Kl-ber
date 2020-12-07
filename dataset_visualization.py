ec5640b4b332e4505032dc70dcc433428e1709d4
Nakiito <34008699+Nakiito@users.noreply.github.com>
2020-12-07 20:35:14 +0100

    Add files via upload

---
+++ dataset_visualization.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 20:23:03 2020
+
+@author: Frédéric
+"""
+
+#this script let me analyse the dataset and visualize its properties
+#will not be useful by itself, I'm just curious :)
+
+import pandas as pd 
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+
+#import data cf dataset_in_out
+
+dataf= pd.read_csv('dataf.csv')
+data1f= pd.read_csv('data1f.csv')
+data2f= pd.read_csv('data2f.csv')
+data3f= pd.read_csv('data3f.csv')
+
+print(dataf.describe())
+
+print('________')
+
+print(dataf.groupby('class').size())
+
+print('________')
+
+#based on "https://machinelearningmastery.com/machine-learning-in-python-step-by-step/"
+
+# box and whisker plots
+dataf.plot(kind='box', subplots=True, layout=(12,12), sharex=False, sharey=False)
+pyplot.show()
+# histograms
+dataf.hist()
+pyplot.show()
+# scatter plot matrix
+scatter_matrix(dataf)
+
+pyplot.show()
